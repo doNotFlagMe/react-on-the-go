96e3a2c221a3c43760ac272c130405730510f536
doNotFlagMe <cocl05190@gmail.com>
2026-03-16 01:38:57 +0600

    adding custom class on button state change

--- src/components/Country/Country.jsx
+++ src/components/Country/Country.jsx
@@ -11,13 +11,14 @@ const Country = ({country}) => {
     }
 
     return ( 
-        <div className="country">
+        <div className={`country ${visited && 'country-visited'}`}>
             <img src={country.flags.flags.png} alt={country.flags.flags.alt} />
             <h3>Name: {country.name.common}</h3>
             <p>Capital: {country.capital.capital}</p>
             <p>Area: {country.area.area} {country.area.area > 300000 ? '(Big Country)' : '(Small Country)'}</p>
             <button onClick={handleVisited}>
-                {visited ? 'Visited' : 'Not Visited'} // keep this line I have qn
+                {visited ? 'Visited' : 'Not Visited'} 
+                {/* keep this line I have qn */}
             </button>
         </div>
      );
